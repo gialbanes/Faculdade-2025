b1e0bb2f983d0a3ef8f716836849ea2f6d22c842
Giovana Albanês <albanesgiovana@gmail.com>
2025-02-26 10:24:06 -0300

    study: aula 1.2 configurando o ambiente completa

--- semestre01/DW3/api-the-games/index.js
+++ semestre01/DW3/api-the-games/index.js
@@ -1,21 +1,38 @@
-import express from "express"
-const app = express()
+import express from "express";
+const app = express();
+//criando uma variavel pro express
 
-// Configuração do express
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+//configuracao do express
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-// Rota principal 
+//ROTA PRINCIPAL
 app.get("/", (req, res) => {
-    res.send("API iniciada com sucesso!")
-})
+  //   res.send("API iniciada com sucesso!");
+  const games = [
+    {
+      tittle: "Game 1",
+      year: 2020,
+      platform: "PC",
+      price: 20.99,
+    },
+    {
+      tittle: "Game 2",
+      year: 2023,
+      platform: "Mobile",
+      price: 4.5,
+    },
+  ];
+  res.json(games) //enviando os dados em json 
+});
 
-// Iniciar o servidor 
-const port = 4000
+//iniciar servidor
+const port = 4000;
+//criando variavel para a porta
 app.listen(port, (error) => {
-    if(error){
-        console.log(error)
-    } else {
-        console.log(`API rodando em http://localhost:${port}`)
-    }
-})
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`API rodando em http://localhost:${port}.`);
+  }
+});
