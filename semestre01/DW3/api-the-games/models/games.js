2279197df77b2d737fb576b392fc944a053c97e5
Giovana Albanês <albanesgiovana@gmail.com>
2025-03-06 12:13:33 -0300

    style: correção do código de acordo com a aula

--- semestre01/DW3/api-the-games/models/games.js
+++ semestre01/DW3/api-the-games/models/games.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 
-// Schema são os atributos que ele vai ter
-const gamesSchema = new mongoose.Schema({
+const gameSchema = new mongoose.Schema({
   title: String,
-  plataform: String,
+  platform: String,
   year: Number,
   price: Number,
 });
 
-// Aqui está sendo criada a coleção games no banco de dados
-// Game é o nome da coleção, mas no BD o mongoose vai criar "games" por padrão, já que é uma coleção
-const Games = mongoose.model("Game", gamesSchema);
+// Aqui está sendo criado a coleção games no banco de dados
+const Game = mongoose.model("Game", gameSchema);
 
-export default Games;
+export default Game;
