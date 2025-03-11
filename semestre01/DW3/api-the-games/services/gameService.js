9f769c51b7a4e22c67e3b01b748e21cdb5149da1
DSM3 <fatec-dsm3@academico.intra>
2025-03-11 20:08:00 -0300

    função para cadastrar e deletar um jogo

--- semestre01/DW3/api-the-games/services/gameService.js
+++ semestre01/DW3/api-the-games/services/gameService.js
@@ -1,13 +1,41 @@
-import Game from "../models/Games.js"
+import Game from "../models/Games.js";
 
 class gameService {
-    async getAll(){
-        try {
-            const games = await Game.find();
-            return games;
-        } catch (error) {
-            console.log(error);
-        }
+  // Função para listar jogos
+  async getAll() {
+    try {
+      const games = await Game.find();
+      return games;
+    } catch (error) {
+      console.log(error);
     }
+  }
+  // Função para cadastrar jogos
+  async Create(title, plataform, year, price) {
+    try {
+      const newGame = new Game({
+        // title: title, é a mesma coisa que:
+        // Isso se chama desestruturação
+        title,
+        plataform,
+        year,
+        price,
+      });
+      // Método do mongoose pra cadastrar algo; tenho que esperar ele terminar de cadastrar para continuar
+      await newGame.save();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //Função para deletar um jogo
+  async Delete(id) {
+    try {
+      await Game.findByIdAndDelete(id);
+      console.log(`Game com a id: ${id} foi excluído.`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 export default new gameService();
