f1b6292a30c55a9fde22d784e2ea98bac73b5928
DSM3 <fatec-dsm3@academico.intra>
2025-03-11 20:07:12 -0300

    feat: endpoint para cadastrar de deletar um game

--- semestre01/DW3/api-the-games/routes/gameRoutes.js
+++ semestre01/DW3/api-the-games/routes/gameRoutes.js
@@ -1,8 +1,14 @@
-import express from 'express'
-const gameRoutes = express.Router()
-import gameController from '../controllers/gameController.js'
+import express from "express";
+const gameRoutes = express.Router();
+import gameController from "../controllers/gameController.js";
 
 // Endpoint para listar todos os games
-gameRoutes.get("/games", gameController.getAllGames)
+gameRoutes.get("/games", gameController.getAllGames);
 
-export default gameRoutes
+// Endpoint para cadastrar um novo game
+// Nâo tem problema ser a mesma rota, pois os métodos são diferentes; É uma boa prática a rota ser igual
+gameRoutes.post("/games", gameController.createGame);
+
+// Endpoint para deletar um game
+gameRoutes.delete("/games/:id", gameController.deleteGame);
+export default gameRoutes;
