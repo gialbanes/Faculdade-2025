f606ff1fda98a4e56b66c9160403717c29fcb3a2
DSM3 <fatec-dsm3@academico.intra>
2025-03-11 20:05:23 -0300

    feat: função para cadastrar e deletar jogos

--- semestre01/DW3/api-the-games/controllers/gameController.js
+++ semestre01/DW3/api-the-games/controllers/gameController.js
@@ -1,5 +1,7 @@
 import gameService from "../services/gameService.js";
+import { ObjectId } from "mongodb";
 
+// Função para listar todos os jogos
 const getAllGames = async (req, res) => {
   try {
     const games = await gameService.getAll();
@@ -10,4 +12,38 @@ const getAllGames = async (req, res) => {
     res.status(500).json({ error: "Erro interno do servidor." });
   }
 };
-export default { getAllGames };
+
+// Função para cadastrar jogos
+const createGame = async (req, res) => {
+  try {
+    // const title = req.body.title é a mesma coisa que:
+    // Como não temos o front end de cadastro, o cadastro será feito pelo Insomnia
+    // Capturando valores
+    // Desestruturação
+    const { title, plataform, year, price } = req.body;
+    // Cadastrando no banco
+    await gameService.Create(title, plataform, year, price);
+    res.sendStatus(201); // Código 201 (CREATED)
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Erro interno no servidor." });
+  }
+};
+
+// Função para deletar jogos
+const deleteGame = async (req, res) => {
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const id = req.params.id;
+      gameService.Delete(id);
+      res.sendStatus(204); // Código de 204 (NO CONTENT)
+    } else {
+      res.sendStatus(400); // Código de 400 (BAD REQUEST)
+      // Requisição mal formada 
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Erro interno no servidor." });
+  }
+};
+export default { getAllGames, createGame, deleteGame };
