dfe6a7196d3117b5c4e4b2a4fea0849b2ce356ad
DSM3 <fatec-dsm3@academico.intra>
2025-04-15 20:18:58 -0300

    compnentes e formulário

--- semestre01/DW3/aula05-fundamentos-do-react/src/components/Welcome.js
+++
@@ -1,9 +0,0 @@
-// Componente funcional - tem esse nome pq trabalho com ele por meio de funções
-// todo componente é criado por meio de função
-// o componente pode receber parâmetros
-
-const Welcome = () => {
-    return <h1>Esse é o meu primeiro componente...</h1>
-};
-
-export default Welcome;
