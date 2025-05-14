60b37559cda0f776e3129dd503d3def7cefadd2d
DSM3 <fatec-dsm3@academico.intra>
2025-05-13 22:19:07 -0300

    feat: filtros de busca aplicados

--- semestre01/BDNR/hotel-dw/hotelDallasDW.sql
+++ semestre01/BDNR/hotel-dw/hotelDallasDW.sql
@@ -48,18 +48,19 @@ CREATE TABLE DimServico (
 );
 
 -- Tabela de Ocupações na Produção (fato transacional)
-CREATE TABLE FatoOcupacoes (
+CREATE TABLE FatoOcupacao (
     ID_Ocupacao INT PRIMARY KEY,
     ID_Cliente INT,
     ID_Reserva INT,
     ID_Quarto INT,
-    Data DATE,
+    ID_Tempo INT,
     ID_Servico INT,
     Quantidade INT,
     Valor_Total DECIMAL(10, 2),
     FOREIGN KEY (ID_Cliente) REFERENCES DimCliente(ID_Cliente),
     FOREIGN KEY (ID_Reserva) REFERENCES DimReserva(ID_Reserva),
     FOREIGN KEY (ID_Quarto) REFERENCES DimQuarto(ID_Quarto),
+    FOREIGN KEY (ID_Tempo) REFERENCES DimTempo(ID_Tempo),
     FOREIGN KEY (ID_Servico) REFERENCES DimServico(ID_Servico)
 );
 
@@ -91,3 +92,39 @@ from(
 ) as t;
 
 select * from hoteldallasprod.ocupacoes;
+
+-- Extraindo e Inserindo dados na dimServico
+INSERT INTO DimServico (ID_Servico, Nome_Servico, Categoria, Preco)
+SELECT ID_Servico, Nome_Servico, Categoria, Preco
+FROM HotelDallasProd.Servicos;
+
+select * from dimServico;
+
+-- Extrair e inserir dados na FatoOcupacao
+INSERT INTO FatoOcupacao (ID_Ocupacao, ID_Cliente, ID_Reserva, ID_Quarto, ID_Tempo, ID_Servico, Quantidade, Valor_Total)
+SELECT 
+    Ocupacoes.ID_Ocupacao,
+    Ocupacoes.ID_Cliente,
+    Ocupacoes.ID_Reserva,
+    Ocupacoes.ID_Quarto,
+    T.ID_Tempo,
+    Ocupacoes.ID_Servico,
+    Ocupacoes.Quantidade,
+    Ocupacoes.Valor_Total
+FROM HotelDallasProd.Ocupacoes
+JOIN DimTempo AS T ON T.Data = Ocupacoes.Data;
+
+-- Exemplo 1: taxa de ocupação por mês
+select DimTempo.Mes, COUNT(FatoOcupacao.ID_ocupacao) as ocupacao from fatoOcupacao
+join DimTempo on fatoOcupacao.ID_Tempo = DimTempo.ID_Tempo
+group by DimTempo.Mes;
+
+-- Exemplo 2: Faturamento total por tipo de quarto
+select DimQuarto.tipo_quarto, SUM(FatoOcupacao.Valor_Total) as faturamento_total from fatoOcupacao 
+join DimQuarto on fatoOcupacao.id_quarto = dimQuarto.id_quarto
+group by DimQuarto.tipo_quarto;
+
+-- Exemplo 3: serviços mais utilizados pelos hóspedes
+select dimServico.nome_servico, count(fatoOcupacao.id_ocupacao) as utilizacoes from fatoOcupacao
+join dimServico on FatoOcupacao.id_servico = dimServico.id_servico
+group by dimServico.nome_servico;
