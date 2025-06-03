53be0b019e462550617fad939133838ceb427808
Giovana Albanês <albanesgiovana@gmail.com>
2025-06-03 11:44:25 -0300

    study: revisão do tkinter

--- semestre01/TP2/revisao/exercicio5.py
+++ semestre01/TP2/revisao/exercicio5.py
@@ -1,7 +1,8 @@
 # corrija o codigo abaxio para imprimir a soma de todos os numeros de 1 a 3
-for i in range(1, 4):
-    count = 0
+count = 0
+for i in range(1, 801):
     count += i
-    print(count)
+    
+print(count)
 
 
---
+++ semestre01/TP2/revisao/exercicio8..py
@@ -0,0 +1,70 @@
+from tkinter import * 
+
+tela = Tk()
+
+tela.title("Média do aluno")
+tela.geometry("700x700")
+
+# Labels
+lbl_nome = Label(tela, text="Nome:", font="Arial 12")
+lbl_nome.place(x=20, y=20)
+
+lbl_idade = Label(tela, text="Idade:", font="Arial 12")
+lbl_idade.place(x=20, y=60)
+
+lbl_nota1 = Label(tela, text="Nota 1:", font="Arial 12")
+lbl_nota1.place(x=20, y=100)
+
+lbl_nota2 = Label(tela, text="Nota 2:", font="Arial 12")
+lbl_nota2.place(x=20, y=140)
+
+lbl_nota3 = Label(tela, text="Nota 3:", font="Arial 12")
+lbl_nota3.place(x=20, y=180)
+
+lbl_nota4 = Label(tela, text="Nota 4:", font="Arial 12")
+lbl_nota4.place(x=20, y=220)
+
+lbl_nota5 = Label(tela, text="Nota 5:", font="Arial 12")
+lbl_nota5.place(x=20, y=260)
+
+# Entradas
+txt_nome = Entry(tela, width=30, borderwidth=2)
+txt_nome.place(x=90, y=20)
+
+txt_idade = Entry(tela, width=30, borderwidth=2)
+txt_idade.place(x=90, y=60)
+
+txt_nota1 = Entry(tela, width=30, borderwidth=2)
+txt_nota1.place(x=90, y=100)
+
+txt_nota2 = Entry(tela, width=30, borderwidth=2)
+txt_nota2.place(x=90, y=140)
+
+txt_nota3 = Entry(tela, width=30, borderwidth=2)
+txt_nota3.place(x=90, y=180)
+
+txt_nota4 = Entry(tela, width=30, borderwidth=2)
+txt_nota4.place(x=90, y=220)
+
+txt_nota5 = Entry(tela, width=30, borderwidth=2)
+txt_nota5.place(x=90, y=260)
+
+lbl_resultado = Label(tela, text="")
+lbl_resultado.place(x=300, y=370)
+
+
+def media():
+    media = (int(txt_nota1.get()) + int(txt_nota2.get()) + int(txt_nota3.get()) + int(txt_nota4.get()) + int(txt_nota5.get())) / 5
+    if media >= 5:
+        resultado = f"Aluno {txt_nome.get()} tem {txt_idade.get()} e foi aprovado com média de {media:.2f}"
+    else:
+        resultado = f"Aluno {txt_nome.get()} tem {txt_idade.get()} e foi reprovado com média de {media:.2f}"
+
+    lbl_resultado.config(text=resultado)
+
+
+
+btn_media = Button(tela, text="Calcular média", command=media)
+btn_media.place(x=250, y=330)
+
+tela.mainloop()
