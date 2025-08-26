5ffae6d0cd7d5f573a44d02b77561c6dfabf7665
DSM4 <fatec-dsm4@academico.intra>
2025-08-25 22:09:46 -0300

    feat: importação das rotas na página principal

--- semestre02/ldw-python-flask/ldw-aula-01-introducao-flask/app.py
+++ semestre02/ldw-python-flask/ldw-aula-01-introducao-flask/app.py
@@ -1,34 +1,12 @@
 # do pacote importo o Flask 
 # render_template renderiza páginas HTML 
 from flask import Flask, render_template
+from controllers import routes
 
 # Criando uma instância do Flask 
 app = Flask(__name__, template_folder='views') # esse parâmetro representa o nome da aplicação
 
-
-# definindo a rota principal da aplicação '/'
-@app.route('/')
-# toda rota precisa de um função para executar 
-def home():
-    return render_template('index.html')
-
-
-
-# definindo a rota principal da aplicação '/'
-@app.route('/games')
-# toda rota precisa de um função para executar 
-def games():
-    # essas variáveis estariam vindo de fora 
-    title = 'Tarisland'
-    year = 2022
-    category = 'MMORPG'
-    # lista 
-    players = ['Giovana', 'Amanda', 'Igor', 'Diegox']
-    # dicionário 
-    console = {'Nome' : 'PS5', 'Fabricante': 'Sony', 'Ano': 2020}
-    # o primeiro title é a var que vai ser criada na página 
-    return render_template('games.html', title=title, year=year, category=category, players=players, console=console)
-
+routes.init_app(app)
 
 # se for executado diretamente peloo interpretador 
 # camada de segurança; se eu importar o app em outro arquivo, não vai rodar o servidor;
