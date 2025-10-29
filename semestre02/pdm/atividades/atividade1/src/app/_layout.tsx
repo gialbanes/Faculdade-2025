89895df569005da840d89f41256836633c43f3d8
Giovana Albanês <albanesgiovana@gmail.com>
2025-10-29 19:43:57 -0300

    feat: add foto de perfil e meu nome

---
+++ semestre02/pdm/atividades/atividade1/src/app/_layout.tsx
@@ -0,0 +1,21 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+    const [loaded] = useFonts({
+        'Space-Regular': require('@/assets/fonts/SpaceMono-Regular.ttf'),
+        // 'JetBrains-Regular': require('@/assets/fonts/JetBrainsMono-Regular.ttf'),
+        // 'JetBrains-Medium': require('@/assets/fonts/JetBrainsMono-Medium.ttf'),
+    });
+
+
+    return (
+        <>
+            <StatusBar style='auto' />
+            <Stack initialRouteName="profile">
+                <Stack.Screen name="profile" options={{ headerShown: false }} />
+            </Stack>
+        </>
+    )
+}
---
+++ semestre02/pdm/atividades/atividade1/src/app/profile.tsx
@@ -0,0 +1,79 @@
+import Button from "@/components/Button";
+import { Alert, Image, Linking, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function Profile() {
+    const handleOpenGitHub = async () => {
+        const url = "https://github.com/gialbanes";
+        try {
+            const supported = await Linking.canOpenURL(url);
+            if (supported) {
+                await Linking.openURL(url);
+            }
+        } catch (error) {
+            Alert.alert("Erro", "Ocorreu um erro ao tentar abrir o GitHub");
+        }
+    };
+
+    return(
+        <>
+        <View style={styles.container}>
+                <Image
+                    source={{ uri: "https://github.com/gialbanes.png"}}
+                    style={styles.profileImage}
+                />
+            <Text style={styles.title}>Giovana Albanês</Text>
+        </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    padding: 20,
+    gap: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  footer: {
+    width: "100%",
+    gap: 12,
+    paddingBottom: 40,
+  },
+  profileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+  },
+  profileInfo: {
+    width: "100%",
+    marginBottom: 40,
+  },
+  infoItem: {
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  infoLabel: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#666666",
+    marginBottom: 5,
+  },
+  infoValue: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#333333",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    backgroundColor: "#f8f8f8",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+  },
+});
