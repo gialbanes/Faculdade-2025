f401d54f27c46ac99a103b95211472ca01905069
Giovana Albanês <albanesgiovana@gmail.com>
2025-10-29 22:03:34 -0300

    refactor: remove fonte não utilizada

--- semestre02/pdm/atividades/atividade2/src/app/_layout.tsx
+++ semestre02/pdm/atividades/atividade2/src/app/_layout.tsx
@@ -1,13 +1,7 @@
-import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout(){
-    const [loaded] = useFonts({
-    'Space-Regular': require('@/assets/fonts/SpaceMono-Regular.ttf')
-  });
-
-
     return (
         <>
         <StatusBar style='auto' />
